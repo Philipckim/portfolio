00412aa8c52a1391d881b3d5ed44b6ae0bd2826a
Philip Kim <philipkim@Philips-MacBook-Air-5.local>
2025-08-17 18:17:39 -0400

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+export const Home = () => {
+  console.log("hiiiii");
+
+  const title = "TITLE HOME PAGE";
+
+  // const buttone = <button style={{backgroundColor: 'red'}}>
+  //     hhhhh
+  // </button>
+
+  return (
+    <div className="min-h-screen container">
+      <h1 id="home_title">Philip's Home Page</h1>
+
+      {/* make this button red */}
+      <button className="button_home" id="button_id">
+        CLICK ME
+      </button>
+      {/* Theme Toggle */}
+
+      {/* Background Effects */}
+
+      {/* Navbar */}
+
+      {/* Main Content */}
+
+      {/* Footer */}
+    </div>
+  );
+};
